d58304b4cf33b910438a2d888fa191bdd9b4e1cd
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-05-01 16:57:51 +0200

    Stage 1/5

--- Who Wants to Be a Millionaire/task/src/main.js
+++ Who Wants to Be a Millionaire/task/src/main.js
@@ -1,6 +1,17 @@
+const URL = 'http://127.0.0.1:8080/question.json';
+const jsonOutput = document.getElementById('container');
 
 const game = async () => {
-  // write your code here
+    // write your code here
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        jsonOutput.innerHTML = JSON.stringify(data);
+    } catch (error) {
+        console.error("Error fetching data", error);
+        jsonOutput.innerHTML = 'Error fetching data';
+    }
+
 }
 
 game();
