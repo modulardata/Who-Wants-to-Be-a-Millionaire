cc754e57f0d2f2ef1929035b8c8349b47547b377
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-11-18 10:45:34 +0100

    Stage 1/5 renew

--- Who Wants to Be a Millionaire/task/src/main.js
+++ Who Wants to Be a Millionaire/task/src/main.js
@@ -1,17 +1,77 @@
-const URL = 'http://127.0.0.1:8080/question.json';
-const jsonOutput = document.getElementById('container');
+(() => {
+    /**
+     * @type {{ question: string, A: string, B: string, C: string, D: string, answer: string }[]}
+     */
+    let questions;
+    const container = document.getElementById('container')
 
-const game = async () => {
-    // write your code here
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        jsonOutput.innerHTML = JSON.stringify(data);
-    } catch (error) {
-        console.error("Error fetching data", error);
-        jsonOutput.innerHTML = 'Error fetching data';
+
+    /**
+     * @return {{question: string, A: string, B: string, C: string, D: string, answer: string}}
+     */
+    function getRandomQuestion() {
+        const idx = Math.floor(Math.random() * questions.length)
+        return questions.splice(idx)[0]
     }
 
-}
+    /**
+     * @param { string } choice
+     * @param { string } answer
+     * @param { number } currentLevel
+     * @return { (event: MouseEvent) => void }
+     */
+    function makeClickHandler(choice, answer, currentLevel) {
+        return (e) => {
+            if (choice === answer) {
+                return start(++currentLevel)
+            }
+            container.innerHTML = '<p>You Lose!</P>'
+        }
+    }
 
-game().then(r =>  console.log(r));
+    /**
+     * @param {{question: string, A: string, B: string, C: string, D: string, answer: string}} question
+     * @param { number } level
+     */
+    function displayQuestion(question, level) {
+        console.log(question)
+        container.innerHTML = `
+            <p>${level}. ${question.question}</P>
+            <ol>
+                <li id="a"">${question.A}</li>
+                <li id="b">${question.B}</li>
+                <li id="c">${question.C}</li>
+                <li id="d">${question.D}</li>
+            </ol>
+        `
+        document.getElementById('a').onclick = makeClickHandler('A', question.answer, level)
+        document.getElementById('b').onclick = makeClickHandler('B', question.answer, level)
+        document.getElementById('c').onclick = makeClickHandler('C', question.answer, level)
+        document.getElementById('d').onclick = makeClickHandler('D', question.answer, level)
+    }
+
+    function startGame() {
+        return start(1)
+    }
+
+    /**
+     * @param { number } level
+     */
+    function start(level) {
+        if (level === 15) {
+            container.innerHTML = '<p>Winner!</P>'
+            return
+        }
+        const question = getRandomQuestion()
+        displayQuestion(question, level)
+    }
+
+    const game = async (uri) => {
+        fetch(uri)
+            .then(r => r.json())
+            .then(q => questions = q)
+            .then(startGame)
+    }
+
+    game('http://127.0.0.1:8080/question.json');
+})()
--- Who Wants to Be a Millionaire/task/test/test.js
+++ Who Wants to Be a Millionaire/task/test/test.js
@@ -1,57 +1,129 @@
 import path from "path";
 import {StageTest, correct, wrong} from "hs-test-web-ts";
+import {doc} from "mocha/lib/reporters/index.js";
 
 const pagePath = path.join(import.meta.url, "../../src/index.html");
 
 class Test extends StageTest {
-  page = this.getPage(pagePath);
+    page = this.getPage(pagePath);
 
-  tests = [
-    // Test 1 - check the open page
-    this.node.execute(async () => {
-      await this.page.open();
-      return correct()
-    }),
+    tests = [
+        // Test 1 - check the open page
+        this.node.execute(async () => {
+            await this.page.open();
+            return correct();
+        }),
 
-    // Test 2 - check container
-    this.page.execute(() => {
-      const container = document.getElementById("container");
-      return container ?
-          correct() :
-          wrong("Not found container. You should create a container with id=container")
-    }),
+        // test 1.1
+        this.page.execute(async () => {
+            // helper function
+            this.getData = () => {
+                return this.data;
+            };
+            this.data = null;
+            this.URL = "http://127.0.0.1:8080/question.json";
+            try {
+                const response = await fetch(this.URL);
+                this.data = await response.json();
+                return correct();
+            } catch (e) {
+                return wrong("Failed to load questions. Make sure you're running the 'http-server --cors' in the directory where the HTML file is located.");
+            }
+        }),
 
-    // Test 3 - check the function game()
-    this.page.execute(() => {
-      try {
-        game();
-        return correct();
-      } catch (e) {
-        return wrong("Not found function game(). Your page must contain a function with 'game()' name.")
-      }
-    }),
+        // Test 2 - check container
+        this.page.execute(() => {
+            const container = document.getElementById("container");
+            return container ?
+                correct() :
+                wrong("Not found container. You should create a container with id=container")
+        }),
 
-    // Test 4 - check the container for contents
-    this.page.execute(async () => {
-      const URL = "http://127.0.0.1:8080/question.json";
+        // Test 3 - check the output questions
+        this.page.execute(async () => {
+            await new Promise(resolve => setTimeout(resolve, 1000));
 
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      const container = document.getElementById("container");
-      const containerText = container.textContent;
+            const container = document.getElementById("container");
+            const containerText = container.textContent;
 
-      return fetch(URL)
-          .then((response) => response.json())
-          .then((data) => {
-            return containerText !== JSON.stringify(data) ?
-                wrong("Expected container text to be questions, but found: " + containerText) :
-                correct();
-          }).catch(() => {
-            return wrong("Failed to load questions. Make sure you're running the 'http-server --cors' in the directory where the HTML file is located.");
-          });
-    })
-  ]
+            let isQuestion = false;
+            for (let i in this.data) {
+                if (containerText.includes(this.data[i].question)) {
+                    isQuestion = true;
+                }
+            }
+            return isQuestion === true ? correct() : wrong("Questions and answer options are not displayed on the page.");
+        }),
+
+        // Test 4 - check switch levels
+        this.node.execute(async () => {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            this.container = await this.page.findById('container');
+            this.index = {'A': 0, 'B': 1, 'C': 2, 'D': 3};
+
+            let data = await this.page.evaluate(() => {
+                return this.getData();
+            });
+
+            this.checkQuestions = async () => {
+                let containerText = await this.container.textContent();
+                const answers = await this.page.findAllBySelector("li");
+                let isEventHappened = false;
+                for (let i in data) {
+                    if (containerText.includes(data[i].question)) {
+                        let indexAnswer = this.index[data[i].answer];
+                        if (!answers[indexAnswer]) return isEventHappened;
+                        await answers[indexAnswer].click();
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        isEventHappened = true;
+                    }
+                }
+                return isEventHappened;
+            }
+            let isEventHappened = await this.checkQuestions();
+            if (isEventHappened === false) {
+                return wrong('Does not proceed to the next question. After clicking on the correct answer, the following question should be displayed.');
+            } else return correct();
+        }),
+
+        // Test 5 -  check output no more than 15 question
+        this.node.execute(async () => {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            let countQuestions = 0;
+
+            let data = await this.page.evaluate(() => {
+                return this.getData();
+            });
+
+            this.checkQuestions = async () => {
+                let containerText = await this.container.textContent();
+                const answers = await this.page.findAllBySelector("li");
+                let isEventHappened = false;
+                for (let i in data) {
+                    if (containerText.includes(data[i].question)) {
+                        countQuestions++;
+                        let indexAnswer = this.index[data[i].answer];
+                        if (!answers[indexAnswer]) return isEventHappened;
+                        await answers[indexAnswer].click();
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        isEventHappened = true;
+                    }
+                }
+                return isEventHappened;
+
+            }
+            while (countQuestions < 13) {
+                let isEventHappened = await this.checkQuestions();
+                if (isEventHappened !== true) {
+                    return wrong('The user\'s answer is incorrectly marked or not 15 questions are displayed.');
+                }
+            }
+            return correct();
+        }),
+    ]
 }
 
 it("Test stage", async () => {
-  await new Test().runTests()
+    await new Test().runTests()
 }).timeout(30000);
